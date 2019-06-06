ae001c888b12fcfb3aaeef2958dbe57c39b429bb
EreminArtem <eremin-artem@inbox.ru>
2019-06-06 20:22:58 +0300

    add packages, bootstrap

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style/App.css';
+import MainView from "./MainView";
+
+function App() {
+    return (
+        <MainView/>
+    );
+}
+
+export default App;
---
+++ src/component/MainView.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+class MainView extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MainView;
